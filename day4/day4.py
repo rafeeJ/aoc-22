78ae6353248b8f216a35245739bdd1747ce84c37
Rafee Jenkins <rafeejenkins@gmail.com>
2022-12-04 10:16:20 +0000

    day4

---
+++ day4/day4.py
@@ -0,0 +1,54 @@
+f = open('input.txt', 'r')
+li = [i.strip() for i in f]
+
+def parse_pair(pair):
+    l_t, r_t = pair.split(',')
+    # get largest range.
+    s, l = l_t.split('-')
+    left_array = [*range(int(s), int(l)+1, 1)]
+    
+    s, l = r_t.split('-')
+    right_array = [*range(int(s), int(l)+1, 1)]  
+
+    return [left_array, right_array]
+
+def check_subarray(larger, smaller):
+    large_idx = 0
+    small_idx = 0
+
+    while ( large_idx < len(larger) and small_idx < len(smaller)):
+        if larger[large_idx] == smaller[small_idx]:
+            large_idx += 1
+            small_idx += 1
+            if small_idx == len(smaller):
+                return True
+        else:
+            large_idx = large_idx - small_idx + 1
+            small_idx = 0
+    return False
+
+def solution_one(input_list):
+    acc = 0
+    for pair in input_list:
+        left, right = parse_pair(pair)
+        if len(left) > len(right):
+            if check_subarray(left, right): acc +=1
+        if len(right) > len(left):
+            if check_subarray(right, left): acc +=1
+        else:
+            if right == left: acc +=1
+    return acc
+
+output = solution_one(li)
+print(output)
+
+def solution_two(input_list):
+    acc = 0
+    for pair in input_list:
+        left, right = parse_pair(pair)
+        if set(left) & set(right):
+            acc +=1
+    return acc
+
+output = solution_two(li)
+print(output)
