37621631a639886d4646b703aa9d19b143fcdd3a
Rafee Jenkins <rafeejenkins@gmail.com>
2022-12-04 10:33:37 +0000

    day4: more pythonic way of checking subarray

--- day4/day4.py
+++ day4/day4.py
@@ -27,14 +27,24 @@ def check_subarray(larger, smaller):
             small_idx = 0
     return False
 
+def check_subarray_pythonic(larger, smaller):
+    try:
+        idx = larger.index(smaller[0]) 
+        new_large = larger[idx:]
+        if len(new_large) >= len(smaller):
+            return True
+        else: return False
+    except ValueError:
+        return False
+
 def solution_one(input_list):
     acc = 0
     for pair in input_list:
         left, right = parse_pair(pair)
         if len(left) > len(right):
-            if check_subarray(left, right): acc +=1
+            if check_subarray_pythonic(left, right): acc +=1
         if len(right) > len(left):
-            if check_subarray(right, left): acc +=1
+            if check_subarray_pythonic(right, left): acc +=1
         else:
             if right == left: acc +=1
     return acc
